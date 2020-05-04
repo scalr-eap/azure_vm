1525064dc44bb4db655a8b7079b6e22008bf1e1c
Ryan <ryan.fee625@gmail.com>
2020-05-04 11:45:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "azurerm_linux_virtual_machine" "example" {
     azurerm_network_interface.example.id,
   ]
 
-  admin_password = "adminuser"
+  admin_password = "ThisIsaPassword123!"
 
   os_disk {
     caching              = "ReadWrite"
